a5774cfe8ec91d91aa0e116ddf3a24bda8b09296
myworldbox <uniletsbefd@gmail.com>
2022-03-27 13:03:03 +0800

    no message

---
+++ build/precache-manifest.8452aa0afc63f6a932ae1669296cdf62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09bd564a0a5043e76fe3263f0dfb53ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da6d5da663531fc99d0b",
+    "url": "/static/css/2.0bcc415f.chunk.css"
+  },
+  {
+    "revision": "2c72f412130c66ec578e",
+    "url": "/static/css/main.829173d8.chunk.css"
+  },
+  {
+    "revision": "da6d5da663531fc99d0b",
+    "url": "/static/js/2.ddbdaef7.chunk.js"
+  },
+  {
+    "revision": "2c72f412130c66ec578e",
+    "url": "/static/js/main.6a0854e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
